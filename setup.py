403cf7ddb76745fc7fe5e1a482ef48c9f638721d
Pål Grønås Drange <pgdr@equinor.com>
2020-11-19 13:41:54 +0100

    Fix setup file

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import setuptools
 setuptools.setup(
     name="workflowruns",
     version="0.0.0",
+    py_modules=['workflowruns'],
     entry_points={"console_scripts": ["workflowruns=workflowruns:main"]},
     install_requires=["pygithub"],
 )
